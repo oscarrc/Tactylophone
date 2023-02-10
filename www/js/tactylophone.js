6c115a371fba59bbef82042fd8b88e72837b3c28
Oscar <oscarrc.web@gmail.com>
2023-02-10 13:58:14 +0100

    [FIX] this

--- www/js/tactylophone.js
+++ www/js/tactylophone.js
@@ -18,67 +18,65 @@ class Tactylophone {
     }
     
     osc = {
-        $: this,
-        play(note){
+        play: (note) => {
             if(!note) return
-            if(!this.$.#osc.enabled) return;
-            if(!this.$.context) this.$.#context = new AudioContext();
+            if(!this.#osc.enabled) return;
+            if(!this.context) this.#context = new AudioContext();
             
-            const freq = this.$.frequencies[note];
-            const osc = this.$.#context.createOscillator();
-            const envelope = this.$.#context.createGain();
+            const freq = this.frequencies[note];
+            const osc = this.#context.createOscillator();
+            const envelope = this.#context.createGain();
             
-            if(this.$.#osc.instance) this.$.#osc.instance.disconnect();
+            if(this.#osc.instance) this.#osc.instance.disconnect();
     
             osc.type = "square";
-            osc.frequency.value = freq * this.$.#osc.tuning;
+            osc.frequency.value = freq * this.#osc.tuning;
             envelope.gain.value = 0.3;
     
             osc.connect(envelope);
-            envelope.connect(this.$.#context.destination);
+            envelope.connect(this.#context.destination);
             osc.start();
             
-            this.$.#osc.instance = osc;
-            this.$.#vibrato.enabled && this.$.vibrato.start();
+            this.#osc.instance = osc;
+            this.#vibrato.enabled && this.vibrato.start();
         },    
-        stop(){
-            if(!this.$.#osc.instance) return;
-            this.$.#osc.instance.stop();
-            this.$.#osc.instance.disconnect(); 
+        stop: () => {
+            if(!this.#osc.instance) return;
+            this.#osc.instance.stop();
+            this.#osc.instance.disconnect(); 
         },
-        toggle(){
-            this.$.#osc.enabled = !this.$.#osc.enabled;
-            if(!this.$.#osc.enabled) this.stop();
+        toggle: () => {
+            this.#osc.enabled = !this.#osc.enabled;
+            if(!this.#osc.enabled) this.osc.stop();
         }
     }
 
     vibrato = {
-        $: this,
-        start(){
-            if(!this.$.#context || !this.$.#osc.instance || !this.$.#osc.enabled) return;
+        start: () => {
+            if(!this.#context || !this.#osc.instance || !this.#osc.enabled) return;
         
-            const oscillator = this.$.#context.createOscillator();
-            const envelopeEffect = this.$.#context.createGain();
+            const oscillator = this.#context.createOscillator();
+            const envelopeEffect = this.#context.createGain();
             
-            if(this.$.#vibrato.instance) this.$.#vibrato.instance.disconnect();
+            if(this.#vibrato.instance) this.#vibrato.instance.disconnect();
     
             oscillator.frequency.value = 5
             envelopeEffect.gain.value = 5;
            
-            oscillator.connect(envelopeEffect).connect(this.$.#osc.instance.frequency);
+            oscillator.connect(envelopeEffect).connect(this.#osc.instance.frequency);
             oscillator.start();
             
-            this.$.#vibrato.instance = oscillator;
+            this.#vibrato.instance = oscillator;
         },
-        stop(){        
-            if(!this.$.#vibrato.instance) return;
-            this.$.#vibrato.instance.stop();
-            this.$.#vibrato.instance.disconnect();
+        stop: () => {        
+            if(!this.#vibrato.instance) return;
+            this.#vibrato.instance.stop();
+            this.#vibrato.instance.disconnect();
         },
-        toggle(){
-            this.$.#vibrato.enabled = !this.$.#vibrato.enabled;
-            if(!this.$.#vibrato.enabled) this.stop();
-            else this.start();
+        toggle: () => {
+            this.#vibrato.enabled = !this.#vibrato.enabled;
+            if(!this.#vibrato.enabled) this.vibrato.stop();
+            else this.vibrato.start();
         }
     }
 
